8a4881a4a0fc8a45ab0f62ead35999590cdbe41d
tpapata1386 <tpapata1386@gmail.com>
2025-11-01 20:45:51 +0330

    xgameruntime: Implement XSystemAnalytics.
    (cherry picked from commit df5b027a12ab1819d193cdbdf1e1f190c49df0ad)

--- dlls/xgameruntime/main.c
+++ dlls/xgameruntime/main.c
@@ -102,6 +102,8 @@ HRESULT WINAPI QueryApiImpl( const GUID *classId, REFIID interfaceId, void **out
         return IXStore6_QueryInterface( x_store_impl, interfaceId, out );
     if (IsEqualGUID( classId, &CLSID_XSystemImpl ))
         return IXSystem5_QueryInterface( x_system_impl, interfaceId, out );
+    if (IsEqualGUID( classId, &CLSID_XSystemAnalyticsImpl ))
+        return IXSystemAnalytics_QueryInterface( x_system_analytics_impl, interfaceId, out );
     if (IsEqualGUID( classId, &CLSID_XThreadingImpl ))
         return IXThreading_QueryInterface( x_threading_impl, interfaceId, out );
     if (IsEqualGUID( classId, &CLSID_XUserImpl ))
